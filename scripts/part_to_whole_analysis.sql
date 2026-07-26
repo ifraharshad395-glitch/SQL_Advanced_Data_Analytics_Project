06e70dd15858977a32ddd3ebe1a4d52cf819fc14
Ifrah Arshad <ifraharshad395@gmail.com>
2026-07-26 17:59:05 +0500

    Update part_to_whole_analysis.sql

--- scripts/part_to_whole_analysis.sql
+++ scripts/part_to_whole_analysis.sql
@@ -1,1 +1,16 @@
+with category_sales as (
+select 
+p.category as category,
+sum(s.sales_amount) as total_sales
+from gold.fact_sales as s left join gold.dim_products as p
+on s.product_key = p.product_key
+group by category
+)
 
+select 
+category,
+total_sales,
+sum(total_sales) over() as overall_sales,
+concat(round((cast(total_sales as float) / sum(total_sales) over() * 100), 2), '%') as percentage_of_total
+from category_sales
+order by total_sales desc
